e0396317a0415987e0cde4edd990bf3101b16ce1
borban <ben@orbanfamily.org>
2024-02-23 17:55:39 -0700

    Add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
 import * as React from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
-import Homepage from './components/Homepage';
-import Travel from './components/Travel';
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import SignIn from './pages/SignIn';
+
 
 export default function App() {
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <Link to="/">
-            <Button variant="contained" disableElevation>
-              Home
-            </Button>
-          </Link>
-          <Link to="/travel">
-            <Button variant="contained" disableElevation>
-              Travel
-            </Button>
-          </Link>
-        </Toolbar>
-      </AppBar>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="travel" element={<Travel />} />
-      </Routes>
+      <SignIn/>
     </div >
   );
 }
