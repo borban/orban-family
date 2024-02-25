ba3a99bfe2da479566bd62bdf18ffc911d880189
borban <ben@orbanfamily.org>
2024-02-25 15:34:16 -0700

    Modified styling for authenticator

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, useTheme, View, Image } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 
@@ -10,9 +10,24 @@ import { Container } from '@mui/material';
 
 Amplify.configure(awsExports);
 
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large} marginTop={'15vh'}>
+        <Image
+          alt="Amplify logo"
+          src="https://docs.amplify.aws/assets/logo-dark.svg"
+        />
+      </View>
+    );
+  }
+}
+
 export default function App() {
   return (
-    <Authenticator hideSignUp={true}>
+    <Authenticator hideSignUp={true} components={components}>
       <Container>
         <TopNavBar />
       </Container>
