cc0feca63a121e36734ac36c79db4723fe23f567
borban <ben@orbanfamily.org>
2024-03-16 12:01:40 -0600

    Successfully able to parse and display connected users

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { Box, TextField, Button, Typography } from '@mui/material';
+import { Box, TextField, Button, Typography, Paper, Grid } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import { getCurrentUser } from 'aws-amplify/auth';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import axios from 'axios';
 
 const ChatBox = () => {
   const [messages, setMessages] = useState([]);
   const [messageInput, setMessageInput] = useState('');
   const [username, setUsername] = useState('');
   const [websocket, setWebsocket] = useState(null);
+  const [connectedClients, setConnectedClients] = useState([]);
 
   useEffect(() => {
     const ws = new W3CWebSocket('wss://jxehlc264l.execute-api.us-east-1.amazonaws.com/production/');
 
     ws.onopen = () => {
       console.log('WebSocket connection opened');
+      fetchConnectedClients(); // Fetch connected clients when WebSocket connection is opened
     };
 
     ws.onmessage = (event) => {
@@ -22,7 +25,6 @@ const ChatBox = () => {
       const message = event.data;
       setMessages((prevMessages) => [...prevMessages, message]);
     };
-    
 
     setWebsocket(ws);
 
@@ -52,32 +54,43 @@ const ChatBox = () => {
     fetchUsername();
   }, []);
 
+  const fetchConnectedClients = async () => {
+    try {
+      const response = await axios.get('https://hoixsu8aca.execute-api.us-east-1.amazonaws.com/prod/connectedClients'); // Adjust the URL based on your API endpoint
+      const bodyArray = JSON.parse(response.data.body);
+      const connectionIdsArray = bodyArray.map(item => item.connectionId);
+      setConnectedClients(connectionIdsArray);
+    } catch (error) {
+      console.error('Error fetching connected clients:', error);
+    }
+  };
+
   const sendMessage = () => {
     if (!websocket) {
       console.error('WebSocket connection not established.');
       return;
     }
-  
+
     const currentTime = new Date(new Date().toLocaleString('en', { timeZone: 'America/Chicago' }));
     const formattedTime = '[' + currentTime.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }) + '] ';
-  
+
     const fullMessage = formattedTime + `${username}: ${messageInput}`;
-  
+
     // Construct the message object
     const messageObject = {
       action: 'sendMessage',
       message: fullMessage
     };
-  
+
     // Stringify the message object
     const finalMessage = JSON.stringify(messageObject);
-    
+
     // Send the message via WebSocket
     websocket.send(finalMessage);
     console.log(finalMessage);
     setMessageInput('');
   };
-  
+
 
   const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
@@ -87,35 +100,50 @@ const ChatBox = () => {
   };
 
   return (
-    <Box>
-      <div style={{ overflowY: 'auto', flex: 1 }}>
+    <Box maxWidth="500px" mx="auto">
+      <Paper elevation={3} style={{ padding: '20px', maxHeight: '60vh', overflowY: 'auto' }}>
         {messages.map((msg, index) => (
           <Typography key={index} variant="body1" gutterBottom>
             {msg}
           </Typography>
         ))}
-      </div>
-      <TextField
-        variant="outlined"
-        margin='dense'
-        fullWidth
-        id="messageInput"
-        label="Type message and hit enter"
-        name="messageInput"
-        value={messageInput}
-        onChange={(e) => setMessageInput(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
-      <Button
-        type="button"
-        onClick={sendMessage}
-        variant="contained"
-        color="primary"
-        endIcon={<SendIcon />}
-        style={{ marginTop: '8px' }}
-      >
-        Send
-      </Button>
+      </Paper>
+      <Typography variant="h6" gutterBottom style={{ marginTop: '20px' }}>
+        Connected Clients:
+      </Typography>
+      <Box>
+        <Typography variant="body1">
+          {connectedClients.map((id, index) => (
+            <div key={index}>{id}</div>
+          ))}
+        </Typography>
+      </Box>
+      <Grid container spacing={2} alignItems="center" style={{ marginTop: '10px' }}>
+        <Grid item xs={9}>
+          <TextField
+            variant="outlined"
+            fullWidth
+            id="messageInput"
+            label="Type message and hit enter"
+            name="messageInput"
+            value={messageInput}
+            onChange={(e) => setMessageInput(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Button
+            type="button"
+            onClick={sendMessage}
+            variant="contained"
+            color="primary"
+            endIcon={<SendIcon />}
+            fullWidth
+          >
+            Send
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
